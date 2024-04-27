495f3de52d2821828b57a41407ecd842a9b2c54a
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-04-28 00:05:45 +0200

    starting Node Webpack Project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Technologies 
+- NodeJS
+- NPM
+- HTML
+- JS 
+- SASS
+- WEBPACK
+
+## How to Start the Project
+
+- Run the `npm install` command to install the dependencies. 
+- Run the `npm run start` command to start the development server.
+
---
+++ index.js
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+console.log("Funcionou!");
+
+
+
+document.getElementById("findButton").addEventListener("click", iniciarExercicioFind);
+
+document.getElementById("filterButton").addEventListener("click", iniciarExercicioFilter);
+
+    function iniciarExercicioFind () {
+        console.log(" Executando find");
+    }
+
+    function iniciarExercicioFilter () {
+        console.log(" Executando filter");
+    }
